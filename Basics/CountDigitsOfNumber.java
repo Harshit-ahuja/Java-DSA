84b034fe4526049e48ddc38bc7a2466503e3c020
Harshit Ahuja <harshit@Harshits-MacBook-Air.local>
2024-10-26 12:31:37 +0530

    Update

---
+++ Basics/CountDigitsOfNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CountDigitsOfNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        // Keeping the count
+        int count = 0;
+        while(n != 0) {
+            n = n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
