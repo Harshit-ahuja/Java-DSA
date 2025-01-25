a88906a754e70353c5688e590384b7f8c46fada6
Harshit Ahuja <harshit@Harshits-MacBook-Air.local>
2025-01-26 01:09:39 +0530

    Update

---
+++ Graphs/IsAGraphConnected.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class IsAGraphConnected {
+    static class Edge{
+        int src;
+        int nbr;
+        int wt;
+        
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for(int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for(int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        // Verifying if the connected component of 0th vertex is equal to the number of vertices in the graph
+        boolean[] visited = new boolean[vtces];
+        ArrayList<Integer> comp = new ArrayList<>();
+        groupConnectedCompTogether(graph, 0, visited, comp);
+
+        System.out.println(comp.size() == vtces);
+    }
+
+    public static void groupConnectedCompTogether(ArrayList<Edge>[] graph, int v, boolean[] visited, ArrayList<Integer> comp) {
+        visited[v] = true;
+        comp.add(v);
+        for(Edge edge : graph[v]) {
+            if(visited[edge.nbr] == false) {
+                groupConnectedCompTogether(graph, edge.nbr, visited, comp);
+            }
+        }
+    }
+}
